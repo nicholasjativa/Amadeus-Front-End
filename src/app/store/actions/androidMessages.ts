223bc2db1d3f0b5380a176c1b3d093bd2aba6c56
Nicholas Jativa <nicholas.jativa@outlook.com>
2018-12-10 20:49:46 -0500

    Created action for sending text message

--- src/app/store/actions/androidMessages.ts
+++ src/app/store/actions/androidMessages.ts
@@ -3,6 +3,13 @@ import { AndroidMessagesActionTypes } from '../action-types/androidMessages';
 import { AndroidMessage } from '../../models/androidMessage';
 import { AmadeusMessageStatus } from '../../models/amadeusMessageStatus';
 
+export class AndroidReceivedAmadeusMessage implements Action {
+    public readonly type: string = AndroidMessagesActionTypes.ANDROID_RECEIVED_AMADEUS_MESSAGE;
+
+    constructor(public payload: AmadeusMessageStatus) {
+    }
+}
+
 export class ReceivedAndroidMessage implements Action {
     public readonly type: string = AndroidMessagesActionTypes.RECEIVED_ANDROID_MESSAGE;
 
@@ -17,9 +24,23 @@ export class ReceivedAmadeusMessageStatus implements Action {
     }
 }
 
-export class AndroidReceivedAmadeusMessage implements Action {
-    public readonly type: string = AndroidMessagesActionTypes.ANDROID_RECEIVED_AMADEUS_MESSAGE;
+export class SendAmadeusMessage implements Action {
+    public readonly type: string = AndroidMessagesActionTypes.SEND_AMADEUS_MESSAGE;
 
-    constructor(public payload: AmadeusMessageStatus) {
+    constructor(public payload: any) {
+    }
+}
+
+export class SendAmadeusMessageSuccess implements Action {
+    public readonly type: string = AndroidMessagesActionTypes.SEND_AMADEUS_MESSAGE_SUCCESS;
+
+    constructor(public payload: any) {
+    }
+}
+
+export class SendAmadeusMessageError implements Action {
+    public readonly type: string = AndroidMessagesActionTypes.SEND_AMADEUS_MESSAGE_ERROR;
+
+    constructor(public payload: any) {
     }
 }
